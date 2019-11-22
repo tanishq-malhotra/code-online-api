352f0c7e67204a705abfd2b68907e9d1117693fc
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2019-11-23 01:43:55 +0530

    c++ programs with no inputs can now be combined

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,7 @@
 import { spawn, exec } from "child_process";
 import path from "path";
+import fs from "fs";
+
 export const apiRoutes = async router => {
   // default welcome route
   router.get("/", async (req, res) => {
@@ -26,5 +28,42 @@ export const apiRoutes = async router => {
         });
       } else res.send(out);
     });
+  });
+
+  router.post("/get-data", async (req, res) => {
+    const code = req.body.info.code;
+    const input = req.body.info.input;
+    let p = path.join(__dirname, "../../files/t.c++");
+    fs.appendFile(p, code, (err) => {
+      if(err) throw err;
+    });
+    
+    let compile = spawn("g++", [p]);
+    let out = '';
+    compile.stderr.on("data", async data => {
+      out += data;
+    });
+    compile.on("close", data => {
+      if (data === 0) {
+        let run = spawn("./a.exe", []);
+
+        
+        // run.stdin.write("" + req.body.n1 + " " + req.body.n2);
+        // run.stdin.end();
+        run.stdout.on("data", output => {
+          fs.unlink(p, (err) => {
+            if(err) throw err;
+          })
+          res.send(out + String(output));
+        });
+      } else 
+          {
+            fs.unlink(p, err => {
+              if (err) throw err;
+            });
+            res.send(out);
+          }
+    });
+
   });
 };
