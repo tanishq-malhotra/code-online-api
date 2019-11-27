b6c1c265df3ae6366f866ee8784dcb7173e0737e
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2019-11-27 15:06:35 +0530

    error in sending the response is fixed

--- src/routes/compilers/cppCompiler.js
+++ src/routes/compilers/cppCompiler.js
@@ -1,43 +1,46 @@
-import {spawn} from 'child_process';
-import path from 'path';
-import fs from 'fs';
+import { spawn } from "child_process";
+import path from "path";
+import fs from "fs";
 
-export const compileCpp = (router) => {
-    router.post('/compile-cpp', async (req, res) => {
-        const code = req.body.code;
-        const input = req.body.input.split('\n');
-        const timeStamp = req.body.timeStamp;
-        const p = path.join(__dirname, '../../../files/' + timeStamp + '.cpp');
-        const filePath = path.join(__dirname, '../../../files/' + timeStamp + '.out');
+export const compileCpp = router => {
+  router.post("/compile-cpp", async (req, res) => {
+    const code = req.body.params.code;
+    const input = req.body.params.input.split("\n");
+    const timeStamp = req.body.params.timeStamp;
+    const p = path.join(__dirname, "../../../files/" + timeStamp + ".cpp");
+    const filePath = path.join(
+      __dirname,
+      "../../../files/" + timeStamp + ".exe"
+    );
 
-        fs.writeFile(p, code, (err) => {
-            if (err) throw err;
-        });
-
-        const compile = spawn('g++', ['-o', filePath, p]);
-        let out = '';
-        compile.stderr.on('data', async (data) => {
-            out += data;
-        });
-        compile.on('close', (data) => {
-            if (data === 0) {
-                const run = spawn(filePath, []);
-
-                [...input].forEach((e) => run.stdin.write(e + ' '));
-                run.stdin.end();
-                run.stdout.on('data', (output) => {
-                    fs.unlink(p, (err) => {
-                        if (err) throw err;
-                    });
-                    fs.unlink(filePath, (err) => {
-                        if (err) throw err;
-                    });
-
-                    res.send(String(output));
-                });
-            } else {
-                res.send(out);
-            }
-        });
+    fs.writeFile(p, code, err => {
+      if (err) throw err;
     });
+
+    const compile = spawn("g++", ["-o", filePath, p]);
+    let out = "";
+    compile.stderr.on("data", async data => {
+      out += data;
+    });
+    compile.on("close", data => {
+      if (data === 0) {
+        const run = spawn(filePath, []);
+
+        [...input].forEach(e => run.stdin.write(e + " "));
+        run.stdin.end();
+        run.stdout.on("data", async output => {
+          fs.unlink(p, (err) => {
+              if (err) throw err;
+          });
+          fs.unlink(filePath, (err) => {
+              if (err) throw err;
+          });
+
+           res.send(String(output));
+          
+        });
+      }
+      else res.send("error");
+    });
+  });
 };
