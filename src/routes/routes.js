7e6c54ff91b00763d00db9c559cc3af8e91f97b5
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2019-11-28 17:33:23 +0530

    new route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,20 @@
-export const apiRoutes = async (router) => {
-    // default welcome route
-    router.get('/', async (req, res) => {
-        res.send('Compiler-API');
-    });
+import { codeCompiler } from "../code-compiler";
+
+export const apiRoutes = async router => {
+  // default welcome route
+  router.get("/", async (req, res) => {
+    res.send("Compiler-API");
+  });
+  router.post("/compile", async (req, res) => {
+    const code = req.body.params.code;
+    const input = req.body.params.input.split("\n");
+    const options = {
+      fileName: req.body.params.timeStamp,
+      input:  req.body.params.input,
+      language: req.body.params.language,
+      timeout: req.body.params.timeout
+    };
+    const output = await codeCompiler(code, options);
+    res.send(output);
+  });
 };
