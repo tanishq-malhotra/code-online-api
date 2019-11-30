ed5ddada3fe978932013bec7f7474052a8293cdd
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2019-11-30 22:42:35 +0530

    es linted

--- src/app.js
+++ src/app.js
@@ -1,33 +1,33 @@
-import express from "express";
-import http from "http";
-import { errorHandler, undefinedRouteHandler } from "./middlewares";
-import path from "path";
-import io from "socket.io";
+import express from 'express';
+import http from 'http';
+import {errorHandler, undefinedRouteHandler} from './middlewares';
+import path from 'path';
+import io from 'socket.io';
 
 class App {
-  constructor() {
-    this.express = express();
-    this.router = express.Router();
-    this.server = http.createServer(this.express);
-    this.io = io(this.server);
-  }
+    constructor() {
+        this.express = express();
+        this.router = express.Router();
+        this.server = http.createServer(this.express);
+        this.io = io(this.server);
+    }
 
-  mountRoutes(routes) {
-    routes(this.router);
-  }
+    mountRoutes(routes) {
+        routes(this.router);
+    }
 
-  start(port) {
-    const app = this.express;
+    start(port) {
+        const app = this.express;
 
-    app.use("/", this.router);
-    app.use(express.static(path.join(__dirname, "../files")));
-    app.use(undefinedRouteHandler);
-    app.use(errorHandler);
+        app.use('/', this.router);
+        app.use(express.static(path.join(__dirname, '../files')));
+        app.use(undefinedRouteHandler);
+        app.use(errorHandler);
 
-    this.server.listen(port, () => {
-      console.log(`Server started on port: ${port}`);
-    });
-  }
+        this.server.listen(port, () => {
+            console.log(`Server started on port: ${port}`);
+        });
+    }
 }
 
 export default new App();
