f42d00b348d6a43d8c3fad499ff82c3fb47386e0
Tanishq Malhotra <tanishqmalhotra@pop-os.localdomain>
2019-11-30 22:42:35 +0530

    es linted

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-import bodyParser from "body-parser";
-import logger from "morgan";
+import bodyParser from 'body-parser';
+import logger from 'morgan';
 
-import app from "./app";
-import { ENV } from "./config";
-const { PORT } = ENV;
-import { apiRoutes, compileCpp } from "./routes";
-import cors from "cors";
-import { codeCompiler } from "./code-compiler";
+import app from './app';
+import {ENV} from './config';
+const {PORT} = ENV;
+import {apiRoutes, compileCpp} from './routes';
+import cors from 'cors';
+import {codeCompiler} from './code-compiler';
 
 app.express.use(cors());
 app.express.use(bodyParser.json());
-app.express.use(bodyParser.urlencoded({ extended: true }));
+app.express.use(bodyParser.urlencoded({extended: true}));
 
-app.express.use(logger("dev"));
+app.express.use(logger('dev'));
 
-app.io.on("connection", async socket => {
-  console.log("client connected");
-  socket.on("compile", async req => {
-    const code = req.code;
-    const options = {
-      fileName: req.params.id,
-      input: req.params.input,
-      language: req.params.language,
-      timeout: req.params.timeout
-    };
-    try {
-      const output = await codeCompiler(code, options);
-      socket.emit(req.params.id, output);
-    } catch (err) {}
-  });
+app.io.on('connection', async (socket) => {
+    console.log('client connected');
+    socket.on('compile', async (req) => {
+        const code = req.code;
+        const options = {
+            fileName: req.params.id,
+            input: req.params.input,
+            language: req.params.language,
+            timeout: req.params.timeout,
+        };
+        try {
+            const output = await codeCompiler(code, options);
+            socket.emit(req.params.id, output);
+        } catch (err) {}
+    });
 });
 
-app.io.on("disconnect", socket => console.log(socket.id + " disconnected"));
+app.io.on('disconnect', (socket) => console.log(socket.id + ' disconnected'));
 
 (async () => {
-  app.mountRoutes(apiRoutes);
-  app.start(PORT);
+    app.mountRoutes(apiRoutes);
+    app.start(PORT);
 })();
