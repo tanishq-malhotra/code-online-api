1af89e0b140034cb09973f2cde01d47ca7995481
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2020-01-06 13:28:46 +0530

    new routes for user login, register, file creation and dir creation added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
-import {DockerSandbox, compilers} from '../code-compiler';
 import {randomBytes} from 'crypto';
+import {userSchema, projectSchema} from '../mongo';
+import fs from 'fs';
 import path from 'path';
 
 export const apiRoutes = async (router) => {
@@ -8,32 +9,74 @@ export const apiRoutes = async (router) => {
         res.send('Compiler-API');
     });
 
-    router.post('/compile', async (req, res) => {
-        const language = req.body.language;
-        const code = req.body.code;
-        const input = req.body.input;
+    router.post('/register', async (req, res) => {
+        const {name, email, pass, gender, age, about} = req.body.data;
+        const id = randomBytes(10).toString('hex');
+        userSchema
+            .create({
+                id: id,
+                name: name,
+                email: email,
+                pass: pass,
+                gender: gender,
+                age: age,
+                about: about,
+            })
+            .then((data) => {
+                const dirPath = path.join(
+                    __dirname,
+                    '../../files/' + id + name.split(' ')[0],
+                );
+                fs.mkdirSync(dirPath, {recursive: true}, (err) => {
+                    if (err) throw err;
+                });
+                res.send('done');
+            })
+            .catch((err) => {
+                console.log(err);
+                res.send([]);
+            });
+    });
 
+    router.post('/login', async (req, res) => {
+        const {email, pass} = req.body.data;
 
-        const query = {
-            timeout_value: 5,
-            path: path.resolve(__dirname, '..') + '/',
-            folder: 'temp/' + randomBytes(10).toString('hex'),
-            vm_name: 'docker_machine',
-            compiler_name: compilers[language].compiler_name,
-            file_name: compilers[language].file_name,
-            code: code,
-            output_command: compilers[language].output_command,
-            languageName: language,
-            e_arguments: compilers[language].e_arguments,
-            stdin_data: input,
-        };
+        userSchema
+            .find({
+                email: email,
+                pass: pass,
+            })
+            .then((data) => {
+                if (data.length) {
+                    res.send('done');
+                } else res.send('nope');
+            })
+            .catch((err) => {
+                console.log(err);
+                res.send('err');
+            });
+    });
 
-        const sandbox = new DockerSandbox(query);
+    router.post('/add', async (req, res) => {
+        const {userId, userName, projectName, type, typeName, typePath} = req.body;
 
-        sandbox.run((data, exec_time, err) => {
-            res.json({output: data,
-                errors: err,
-                time: exec_time});
-        });
+        if (type === 'dir') {
+            const dirPath = path.join(__dirname,
+                '../../files/' + userId + userName.split(' ')[0], typePath, typeName,
+            );
+            fs.mkdirSync(dirPath, {recursive: true}, (err) => {
+                if (err) throw err;
+            });
+            res.send('dir created');
+        } else {
+            const filePath = path.join(__dirname,
+                '../../files/' + userId + userName.split(' ')[0], typePath, typeName,
+            );
+
+            fs.writeFile(filePath, '', (err) => {
+                if (err) throw err;
+                res.send('file created');
+            });
+        }
     });
 };
